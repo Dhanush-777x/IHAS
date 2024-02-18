6174d4fe7bc4089edb3d71d891c54ceb8c119795
Dhanush <dhanushmani1504@gmail.com>
2024-02-18 15:25:23 +0530

    home page animations

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,67 +1,91 @@
-// Home.js
-
 import React from 'react';
 import { Link } from "react-router-dom";
 import './Home.css';
 import News from './News';
 import FAQ from './Faq';
 import Footer from './Footer';
+import { motion } from 'framer-motion';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { faUserMd } from '@fortawesome/free-solid-svg-icons/faUserMd';
 import { faFlask } from '@fortawesome/free-solid-svg-icons/faFlask';
 
-
 function Home() {
     return (
         <div>
-            <div id='Section1' className="app-container relative flex justify-center items-center px-10">
+            <motion.div
+                id='Section1'
+                className="app-container relative flex justify-center items-center px-10"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1 }}
+            >
                 <h1 className="hero-text font-bold text-white levitate">Empowering Health: Connecting Patients, Doctors, and Researchers for a Better Tomorrow</h1>
                 <div className="circle rounded-full"></div>
-            </div>
+            </motion.div>
 
-            <div id='Section2' className='features-section flex flex-col justify-center items-center'>
+            <motion.div
+                id='Section2'
+                className='features-section flex flex-col justify-center items-center'
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1, delay: 0.5 }}
+            >
                 <h2 className="features-heading text-4xl font-semibold text-gray-800 mb-20">Features</h2>
                 <div className='flex flex-wrap justify-center'>
 
-                    <div className="card glassmorphism mx-3 mb-4 sm:w-80">
+                    <motion.div
+                        className="card glassmorphism mx-3 mb-4 sm:w-80"
+                        whileHover={{ scale: 1.1 }}
+                    >
                         <div className="icon-container">
                             <FontAwesomeIcon icon={faUser} className="features-icon" />
                         </div>
                         <h2 className="text-lg font-semibold mb-2">Patients</h2>
                         <p className="text-gray-700 mb-8">Access comprehensive information on diseases, symptoms, vaccinations, and nearby medical centers.</p>
-                        <Link to="/diseases" className="glassmorphism-button py-2 my-10 mx-10 text-center text-gray-700 hover:text-black lg:border-b lg:border-gray-100 lg:hover:bg-transparent lg:hover:black">
+                        <Link to="/diseases" className="glassmorphism-button py-2 my-10 mx-10 text-center text-gray-700 hover:text-black hover:shadow-2xl lg:border-b lg:border-gray-100 lg:hover:bg-transparent lg:hover:black"
+                            whileHover={{ scale: 1.05 }}
+                        >
                             View More
                         </Link>
-                    </div>
+                    </motion.div>
 
-                    <div className="card glassmorphism mx-3 mb-4 sm:w-80">
+                    <motion.div
+                        className="card glassmorphism mx-3 mb-4 sm:w-80"
+                        whileHover={{ scale: 1.1 }}
+                    >
                         <div className="icon-container">
                             <FontAwesomeIcon icon={faUserMd} className="features-icon" />
                         </div>
                         <h2 className="text-lg font-semibold mb-2">Doctors</h2>
                         <p className="text-gray-700 mb-8">Explore advanced medical equipment and surgical procedures for effective patient care.</p>
-                        <Link to="" className="glassmorphism-button py-2 my-10 mx-10 text-center text-gray-700 hover:text-black lg:border-b lg:border-gray-100 lg:hover:bg-transparent lg:hover:black">
+                        <Link to="" className="glassmorphism-button py-2 my-10 mx-10 text-center text-gray-700 hover:text-black hover:shadow-2xl lg:border-b lg:border-gray-100 lg:hover:bg-transparent lg:hover:black"
+                            whileHover={{ scale: 1.05 }}
+                        >
                             View More
                         </Link>
-                    </div>
+                    </motion.div>
 
-                    <div className="card glassmorphism mx-3 mb-4 sm:w-80">
+                    <motion.div
+                        className="card glassmorphism mx-3 mb-4 sm:w-80"
+                        whileHover={{ scale: 1.1 }}
+                    >
                         <div className="icon-container">
                             <FontAwesomeIcon icon={faFlask} className="features-icon" />
                         </div>
                         <h2 className="text-lg font-semibold mb-2">Researchers</h2>
                         <p className="text-gray-700 mb-9">Access a repository of research papers and contribute to advancements in medical science.</p>
-                        <Link to="" className="glassmorphism-button py-2 my-10 mx-10 text-center text-gray-700 hover:text-black lg:border-b lg:border-gray-100 lg:hover:bg-transparent lg:hover:black">
+                        <Link to="" className="glassmorphism-button py-2 my-10 mx-10 text-center text-gray-700 hover:text-black hover:shadow-2xl lg:border-b lg:border-gray-100 lg:hover:bg-transparent lg:hover:black"
+                            whileHover={{ scale: 1.05 }}
+                        >
                             View More
                         </Link>
-                    </div>
+                    </motion.div>
                 </div>
-            </div>
+            </motion.div>
             <News/>
             <FAQ />
             <Footer />
-
         </div>
     );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,11 @@
-// Navbar.js
 import React, { useState, useEffect } from "react";
-import './Navbar.css';
-import logo from '../Assets/logo.png';
+import "./Navbar.css";
+import logo from "../Assets/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faComments } from "@fortawesome/free-solid-svg-icons/faComments";
 import { Link } from "react-router-dom";
-import { HashLink } from 'react-router-hash-link';
+import { HashLink } from "react-router-hash-link";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -17,22 +17,35 @@ const Navbar = () => {
       setIsScrolled(!isTop);
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+  const variants = {
+    open: { opacity: 1, height: "auto" },
+    closed: { opacity: 0, height: 0 },
+  };
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
   return (
-    <nav className={`fixed top-0 z-10 w-full py-7 bg-secondary ${isScrolled ? 'lg:bg-secondary shadow-lg' : 'lg:bg-transparent'}`}>
+    <motion.nav
+      className={`fixed top-0 z-10 w-full py-7 bg-secondary ${
+        isScrolled ? "lg:bg-secondary shadow-lg" : "lg:bg-transparent"
+      }`}
+    >
       <div className="flex items-center justify-between w-full mx-auto px-4">
-        <Link to="/" className="flex items-center text-gray-700 hover:text-black"> {/* Use Link instead of anchor tag */}
-          <img className="h-12 w-auto mx-3 pr-2" src={logo} alt="Logo" />
+        <Link to="/" className="flex items-center text-gray-700 hover:text-black">
+          <img
+            className="h-12 w-auto mx-3 pr-2"
+            src={logo}
+            alt="Logo"
+          />
           <span className="text-xl font-semibold">IHAS</span>
         </Link>
         <div className="hidden lg:flex items-center justify-center flex-1">
@@ -46,30 +59,43 @@ const Navbar = () => {
           <HashLink to="/faq#faqSection" className="py-3 px-5 text-center text-gray-600 hover:text-black">FAQ</HashLink>
           <HashLink to="/contact-us#footerSection" className="py-3 px-5 text-center text-gray-600 hover:text-black">Contact Us</HashLink>
           </div>
-        <button 
+        <motion.button
           onClick={toggleMenu}
           className="text-gray-700 hover:text-black focus:outline-none lg:hidden shadow-lg"
           aria-label={isOpen ? "Close menu" : "Open menu"}
+          whileTap={{ scale: 0.9 }}
         >
-          <svg 
-            className="w-6 h-6" 
-            fill="currentColor" 
-            viewBox="0 0 20 20" 
+          <svg
+            className="w-6 h-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path 
+            <path
               fillRule="evenodd"
               d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
               clipRule="evenodd"
             ></path>
           </svg>
-        </button>
-        <Link to="/chat" className="glassmorphism-button hidden lg:block ml-4">
+        </motion.button>
+        <Link
+          to="/chat"
+          className="glassmorphism-button hidden lg:block ml-4"
+        >
           <FontAwesomeIcon icon={faComments} className="mr-2" />
           Chat with Prakriti
         </Link>
-      </div>
-      <div className={`flex flex-col lg:flex-row w-full lg:w-auto p-10 ${isOpen ? 'block' : 'hidden'}`} id="mobile-menu-2">
+        </div>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            className="flex flex-col lg:flex-row w-full lg:w-auto p-10"
+            id="mobile-menu-2"
+            initial={{ opacity: 0, height: 0 }}
+            animate={{ opacity: 1, height: "auto" }}
+            exit={{ opacity: 0, height: 0 }}
+            transition={{ duration: 0.3 }}
+          >
         <Link to="/home" className="py-2 pl-3 pr-4 text-center text-gray-700 hover:text-black lg:border-b lg:border-gray-100 lg:hover:bg-transparent lg:hover:black">Home</Link> 
         <Link to="/" className="py-2 pl-3 pr-4 text-center text-gray-700 hover:text-black lg:border-b lg:border-gray-100 lg:hover:bg-transparent lg:hover:black">Patient's Corner</Link>
         <Link to="/" className="py-2 pl-3 pr-4 text-center text-gray-700 hover:text-black lg:border-b lg:border-gray-100 lg:hover:bg-transparent lg:hover:black">Healthcare Providers</Link>   
@@ -81,8 +107,10 @@ const Navbar = () => {
           <FontAwesomeIcon icon={faComments} className="mr-2" />
           Chat with Prakriti
         </Link>
-      </div>
-    </nav>
+        </motion.div>
+        )}
+      </AnimatePresence>
+    </motion.nav>
   );
 };
 
